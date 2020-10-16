56afed98d9605f85fe030213c7e9fabe60ed9117
mizukiit0 <m-ito1270@st.kyoto-art.ac.jp>
2020-10-16 15:39:39 +0900

    jsの練習

---
+++ sample/js/script02.js
@@ -0,0 +1,31 @@
+$(function(){
+
+
+    let txt1 = "おはようございます！";
+    let txt2 = "おげんきですか？";
+    let num1 = 20;
+    let num2 = 32;
+
+    $("#button01").on("click",function(){
+        $("#content").append("こんにちは！");
+    })
+
+    $("#button02").on("click",function(){
+        $("#content").append(txt1);
+    })
+
+    $("#button03").on("click",function(){
+        $("#content").append(txt1 + txt2);
+    })
+
+    $("#button04").on("click",function(){
+        $("#content").text(num1 + num2);
+    })
+
+    $("#button05").on("click",function(){
+        $("#content").text(num1 + "かける" + num2 + "は" + (num1 * num2) + "です。");
+    })
+
+
+
+})
